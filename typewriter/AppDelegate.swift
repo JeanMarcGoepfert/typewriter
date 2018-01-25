f8c4368693a3e645ce56aec6898d73cc260c1cf7
JeanMarcGoepfert <goepfertjeanmarc@gmail.com>
2018-01-26 10:04:59 +1100

    Initial Commit

---
+++ typewriter/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  typewriter
+//
+//  Created by Jean-Marc Goepfert on 26/1/18.
+//  Copyright © 2018 Jean-Marc Goepfert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
